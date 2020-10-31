7bad97e51b12b40aaa80b63604d2fb0b0ab9827b
Stefano Prosperi <step.prosperi@gmail.com>
2020-10-31 02:42:45 +0100

    Added constructor for class UserModel

--- JigsawBot/DataBase/Models/UserModel.cs
+++ JigsawBot/DataBase/Models/UserModel.cs
@@ -2,10 +2,19 @@
 {
     public class UserModel
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public int Solved { get; set; }
-        public int Score { get; set; }
-        public bool HideSolved { get; set; }
+        public string Id         { get; set; }
+        public string Name       { get; set; }
+        public int    Solved     { get; set; }
+        public int    Score      { get; set; }
+        public bool   HideSolved { get; set; }
+
+        public UserModel(string id, string name)
+        {
+            Id         = id;
+            Name       = name;
+            Solved     = 0;
+            Score      = 0;
+            HideSolved = true;
+        }
     }
 }
